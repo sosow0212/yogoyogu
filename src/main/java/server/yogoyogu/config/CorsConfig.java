ead3c2493bc9c3c16f59752be3744800a8d7ad7b
sosow0212 <sosow0212@naver.com>
2022-10-08 23:30:42 +0900

    fix: fix CORS error

--- src/main/java/server/yogoyogu/config/CorsConfig.java
+++ src/main/java/server/yogoyogu/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("^https?:\\/\\/main—chimerical-malabi-ffde60.netlify.app$"); // e.g. /**, http://domain1.com
+        config.addAllowedOrigin("^https?:\\/\\/https://main--chimerical-malabi-ffde60.netlify.app/$"); // e.g. /**, http://domain1.com
         config.addAllowedHeader(CorsConfiguration.ALL);
         config.addAllowedMethod(HttpMethod.GET);
         config.addAllowedMethod(HttpMethod.POST);
